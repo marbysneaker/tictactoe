a20a29a10205427204b3358583bec0b57bb89843
marby <marbysneaker@gmail.com>
2020-04-14 03:41:00 -0700

    almost done

--- js/main.js
+++ js/main.js
@@ -11,21 +11,58 @@ function render(){
     }
     let x = 'X'
     let o = 'O'
-    let playerOne = x
-    let playerTwo = o
+    let playerOne = []
+    let playerTwo = []
+    function checkForWin(){
+        console.log(playerOne)
+        console.log(playerTwo)
+        let checker = (arr, target) => target.every(v => arr.includes(v));
+        let win1 = ['box1','box2','box3']
+        let win2 = ['box4','box5','box6']
+        let win3 = ['box7','box8','box9']
+        let win4 = ['box1','box4','box7']
+        let win5 = ['box2','box5','box8']
+        let win6 = ['box3','box6','box9']
+        let win7 = ['box1','box5','box9']
+        let win8 = ['box3','box5','box7']
+        if(checker(playerOne, win1) || checker(playerOne, win2) || checker(playerOne, win3) || checker(playerOne, win4) || checker(playerOne, win5) || checker(playerOne, win6) || checker(playerOne, win7) || checker(playerOne, win8)){
+            
+            alert('player One is the winner')
+        }
+        if(checker(playerTwo, win1) || checker(playerTwo, win2) || checker(playerTwo, win3) || checker(playerOne, win4) || checker(playerTwo, win5) || checker(playerTwo, win6) || checker(playerTwo, win7) || checker(playerTwo, win8)){
+            
+            alert('player Two is the winner')
+        }
+        if(checker(playerOne, win1) || checker(playerOne, win2) || checker(playerOne, win3) || checker(playerOne, win4) || checker(playerOne, win5) || checker(playerOne, win6) || checker(playerOne, win7) || checker(playerOne, win8)){
+            
+            alert('player One is the winner')
+        }
+
+
+        
+
+    }
     function displayLetter(box){
         
         if (!clickedBoxes.includes(box)){
             let clicked = document.querySelector(`#${box}`)
             let letter = document.createElement('span')
             clicked.appendChild(letter)
-            letter.textContent = playerOne
+            letter.textContent = x
             clickedBoxes.push(box)
+            playerOne.push(box)
             let newSet = boxes.filter(x => !clickedBoxes.includes(x));
             console.log(newSet)
             let randomBox = newSet[Math.floor(Math.random()*newSet.length)];
             console.log(randomBox)
             clickedBoxes.push(randomBox)
+            playerTwo.push(randomBox)
+            let oponentClicked = document.querySelector(`#${randomBox}`)
+            let opponentLetter = document.createElement('span')
+            oponentClicked.appendChild(opponentLetter)
+            opponentLetter.textContent = o
+            checkForWin()
+            
 
         }
        
